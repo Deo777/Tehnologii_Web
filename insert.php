14586ebd79de117e2b08df84cf2868b53c1b74bd
Deo777 <98517071+Deo777@users.noreply.github.com>
2022-05-25 20:40:16 +0300

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Insert Page page</title>
+</head>
+
+<body>
+	<center>
+		<?php
+
+		// servername => localhost
+		// username => root
+		// password => empty
+		// database name => datele
+		$conn = mysqli_connect("localhost", "root", "", "datele");
+		
+		// Check connection
+		if($conn === false){
+			die("ERROR: Could not connect. "
+				. mysqli_connect_error());
+		}
+		
+		// Taking all 5 values from the form data(input)
+		$first_name = $_REQUEST['first_name'];
+		$password = $_REQUEST['psw'];
+		$email = $_REQUEST['email'];
+		
+		// Performing insert query execution
+		// here our table name is college
+		$sql = "INSERT INTO users VALUES ('$first_name','$email', '$password')";
+		
+		if(mysqli_query($conn, $sql)){
+			echo "<h3>data stored in a database successfully."
+				. " Please browse your localhost php my admin"
+				. " to view the updated data</h3>";
+
+		} else{
+			echo "ERROR: Hush! Sorry $sql. "
+				. mysqli_error($conn);
+		}
+		
+		// Close connection
+		mysqli_close($conn);
+		?>
+	</center>
+</body>
+
+</html>
